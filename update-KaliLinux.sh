036fbdc683029730e6a48eea90c4a20256508478
Mohsen jamal <74454882+MohsenJamal@users.noreply.github.com>
2022-07-14 12:57:16 +0200

    Add files via upload

---
+++ update-KaliLinux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+figlet "Blak X"
+
+echo -e "Kali Linux Upadte Script"
+
+echo "press Enter to continue, CTRL+C to exit."
+
+read input
+
+echo -e "\e[1;36m[+] Updating Kali, Please wait.\e[0m"
+
+apt-get update -y > /dev/null && apt-get upgrade -y > /dev/null && apt-get dist-upgrade -y > >
+echo -e "Kali is now up to date. \n"
+
+echo -e  "\e[1;36m[+] Updating Kali, Please wait.\e[0m"
+
+apt-get autoclean -y > /dev/null && apt-get clean -y > /dev/null
+echo -e "\e[1;36m[+] Kali is now less dirty.."
+echo -e "\n \e[1;36m[+] Thanks to use Script .."
