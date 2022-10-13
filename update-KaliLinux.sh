2b25bf56d1e7c9af5a1498169cbe81be107ece98
Blaxk Raven <74454882+MohsenJamal@users.noreply.github.com>
2022-10-13 11:24:58 +0200

    Update update-KaliLinux.sh

--- update-KaliLinux.sh
+++ update-KaliLinux.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-clear
-figlet "Blak X"
+figlet "AutoUpdateKali"
 
-echo -e "Kali Linux Upadte Script"
-
-echo "press Enter to continue, CTRL+C to exit."
-
+echo -e "Bl@xkR@ven\n"
+echo "$(tput bold)Press ENTER to continue, CTRL+C to abort."
 read input
 
-echo -e "\e[1;36m[+] Updating Kali, Please wait.\e[0m"
+echo -e "\e[1;36m[+] Updating Kali. Please wait.\e[0m"
+apt-get update -y > /dev/null && apt-get upgrade -y > /dev/null && apt-get >
+echo -e "Kali is now up to date.\n"
 
-apt-get update -y > /dev/null && apt-get upgrade -y > /dev/null && apt-get dist-upgrade -y > >
-echo -e "Kali is now up to date. \n"
+echo -e "\e[1;36m[+] Cleaning Kali. Please wait.\e[0m"
 
-echo -e  "\e[1;36m[+] Updating Kali, Please wait.\e[0m"
 
 apt-get autoclean -y > /dev/null && apt-get clean -y > /dev/null
-echo -e "\e[1;36m[+] Kali is now less dirty.."
-echo -e "\n \e[1;36m[+] Thanks to use Script .."
+<adeleon415"
+
