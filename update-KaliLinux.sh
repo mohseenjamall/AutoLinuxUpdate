580102ff45dc3f9a7e4021a6172dce96e275d3e1
Blaxk Raven <74454882+MohsenJamal@users.noreply.github.com>
2022-10-18 09:22:58 +0200

    Update update-KaliLinux.sh

--- update-KaliLinux.sh
+++ update-KaliLinux.sh
@@ -2,17 +2,44 @@
 
 figlet "AutoUpdateKali"
 
-echo -e "Bl@xkR@ven\n"
-echo "$(tput bold)Press ENTER to continue, CTRL+C to abort."
-read input
+clear
+echo "Description: This Bash script will update, upgrade current or prior Kali installations."
+echo "" && sleep 3
+echo "Updating sources.list..."  && sleep 4
 
-echo -e "\e[1;36m[+] Updating Kali. Please wait.\e[0m"
-apt-get update -y > /dev/null && apt-get upgrade -y > /dev/null && apt-get >
-echo -e "Kali is now up to date.\n"
+cat <<EOF > /etc/apt/sources.list 
+# Update /etc/apt/sources.list
+deb http://http.kali.org/kali kali-rolling main contrib non-free
+# For source package access, uncomment the following line
+deb-src http://http.kali.org/kali kali-rolling main contrib non-free
+# For legacy Sana repositories activate these
+#deb http://http.kali.org/kali sana main non-free contrib
+#deb http://security.kali.org/kali-security sana/updates main contrib non-free
+# For source package access, uncomment the following line
+#deb-src http://http.kali.org/kali sana main non-free contrib
+#deb-src http://security.kali.org/kali-security sana/updates main contrib non-free
+EOF
 
-echo -e "\e[1;36m[+] Cleaning Kali. Please wait.\e[0m"
+##;// Main
+function Main {
+    echo "Fixing prior install issues..."  && sleep 4 && \
+    apt-get -f install -fy
+    echo "Updating sources from repositories..."  && sleep 4 && \
+    apt-get update
+    echo "Upgrading installed sources..."  && sleep 4 && \
+    apt-get upgrade -fy
+    echo "Upgrading distribution from repositories..."  && sleep 4 && \
+    apt-get dist-upgrade -y
+    echo "Automatically removing unused sources..."  && sleep 4 && \
+    apt-get autoremove -fy
+    echo "Automatically cleaning up cached sources..."  && sleep 4 && \
+    apt-get autoclean -y
+    echo "Using dpkg to auto-configure packages..."  && sleep 4 && \
+    dpkg --configure -a
+}
 
+Main 
 
-apt-get autoclean -y > /dev/null && apt-get clean -y > /dev/null
+exit 0
 
 echo -e "\e[1;36m[+] Kali is now less dirty... Follow me on Twitter @MohsenJamall"
