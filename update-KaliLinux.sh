08cb662d994f312a3b22e8ab9d6a32d4a0365666
Blaxk Raven <74454882+MohsenJamal@users.noreply.github.com>
2022-10-18 09:14:14 +0200

    Update update-KaliLinux.sh

--- update-KaliLinux.sh
+++ update-KaliLinux.sh
@@ -7,12 +7,12 @@ echo "$(tput bold)Press ENTER to continue, CTRL+C to abort."
 read input
 
 echo -e "\e[1;36m[+] Updating Kali. Please wait.\e[0m"
-apt-get update -y > /dev/null && apt-get upgrade -y > /dev/null && apt-get >
+sudo apt-get update -y > /dev/null && apt-get upgrade -y > /dev/null && apt-get >
 echo -e "Kali is now up to date.\n"
 
 echo -e "\e[1;36m[+] Cleaning Kali. Please wait.\e[0m"
 
 
-apt-get autoclean -y > /dev/null && apt-get clean -y > /dev/null
+sudo apt-get autoclean -y > /dev/null && apt-get clean -y > /dev/null
 
 echo -e "\e[1;36m[+] Kali is now less dirty... Follow me on Twitter @MohsenJamall"
